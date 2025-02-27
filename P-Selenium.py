f2e9219d7cbae5ae187c3a5271f8a94e9d08b353
Pedro M. <pmartinasi@gmail.com>
2025-02-27 20:39:06 +0100

    20250227 - webdriver_manager added

--- P-Selenium.py
+++ P-Selenium.py
@@ -1,12 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
 
 # Set up Firefox to run headlessly
 options = Options()
 options.headless = True  # Run in headless mode
 
-# Initialize WebDriver with the geckodriver path
-driver = webdriver.Firefox(executable_path='/path/to/geckodriver', options=options)
+# Use the GeckoDriverManager to automatically download and set up geckodriver
+service = Service(GeckoDriverManager().install())
+
+# Initialize WebDriver with the service object
+driver = webdriver.Firefox(service=service, options=options)
 
 # Open a website
 driver.get('https://www.mozilla.org')
